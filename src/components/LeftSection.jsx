0ddca5960a9dd4637289f4cf7b9f5a6d919a22ee
DurgeshJhalwa2805 <durgeshjhalwa2805@gmail.com>
2024-07-22 23:41:01 +0530

    mobile player responsive

--- src/components/LeftSection.jsx
+++ src/components/LeftSection.jsx
@@ -7,15 +7,15 @@ import CrossIcon from "../assets/icons/crossIcon.svg"
 
 
 const LeftSection = (props) => {
-    const {isSidebarOpen,toggleSidebar} = props
+    const { isSidebarOpen, toggleSidebar } = props
 
     return (
         <div className="flex md:flex-col h-full justify-between items-center md:items-start p-4 px-8 md:px-6 2xl:p-6">
             <div className=" md:hidden  backdrop-blur-2xl  hover:brightness-150 transition-all duration-300 ease-in-out rounded-full" onClick={toggleSidebar}>
-                <img src={isSidebarOpen ?CrossIcon :PlaylistIcon} alt="Playlist Logo" className="w-8 rounded-full p-1" />
+                <img src={isSidebarOpen ? CrossIcon : PlaylistIcon} alt="Playlist Logo" className="w-8 rounded-full p-1" />
             </div>
             <div>
-                <img src={SpotifyLogo} alt="Spotify Logo" className="w-32 md:w-24 2xl:w-34" />
+                <img src={SpotifyLogo} alt="Spotify Logo" className="w-28 md:w-24 2xl:w-34" />
             </div>
             <div>
                 <img src={ProfileIcon} alt="User Logo" className="w-10  md:w-12 rounded-full" />
