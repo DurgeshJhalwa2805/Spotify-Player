50b96098a7d504fbf0118c021c7996684180dada
DurgeshJhalwa2805 <durgeshjhalwa2805@gmail.com>
2024-07-22 23:28:15 +0530

    mobile player responsive

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,23 @@ import { useState } from 'react'
 
 const App = () => {
   useApplyThemeColors();
-  
+
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
-  console.log(isSidebarOpen,"isSidebarOpen")
+  console.log(isSidebarOpen, "isSidebarOpen")
 
   return (
     <div className='h-screen w-screen custom-gradient  text-white flex flex-col md:flex-row'>
       <div className='w-full md:w-1/6 '>
         <LeftSection isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
       </div>
-      <div className={`transform transition-transform duration-300 ${isSidebarOpen ? "w-full flex translate-x-0":" hidden "} md:w-2/6  md:flex justify-end `}>
+      <div className={`transform transition-transform duration-300 ${isSidebarOpen ? "w-full flex translate-x-0" : "hidden"} md:w-2/6  md:flex justify-end `}>
         <SongList isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
       </div>
-      <div className={`h-full w-full md:w-3/6 ${isSidebarOpen && "hidden"} md:block `}>
+      <div className={`h-full w-full md:w-3/6 ${isSidebarOpen ? "hidden" : "block"} md:block `}>
         <Player />
       </div>
     </div>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -11,84 +11,84 @@ import speakerIcon from "../assets/icons/speakerIcon.svg"
 
 
 const Player = () => {
-    const { currentSong ,audioRef , seekBg,seekBar,playStatus,play,pause,previous,nextSong,seekSong} = useMusic();
-    console.log(currentSong,"current")
+    const { currentSong, audioRef, seekBg, seekBar, playStatus, play, pause, previous, nextSong, seekSong } = useMusic();
+    console.log(currentSong, "current")
 
-    if(!currentSong || Object.keys(currentSong).length ==0){
+    if (!currentSong || Object.keys(currentSong).length == 0) {
         return (<div className="h-full w-full flex justify-center items-center">
             Please select a song !
         </div>)
     }
 
     return (
-      <div className="h-full max-h-screen  w-full flex justify-center   items-start pt-[4vh] md:justify-start  md:items-center  2xl:items-start 2xl:pt-[13vh] md:pl-28 2xl:pl-52">
-        <audio  ref={audioRef} src={currentSong.url} preload="auto">
-        </audio>
-        <div className="flex flex-col text-white p-4 rounded-lg w-[280px] 2xl:w-[325px]">
-        <div className="text-left mb-4">
-            <p className="text-xl 2xl:text-2xl font-semibold">{currentSong.name}</p>
-            <p className="opacity-60 text-xs 2xl:text-sm">{currentSong.artist}</p>
-        </div>
-          <div className="vignette-container h-[250px] 2xl:h-[300px] w-full mb-4 rounded-md overflow-hidden">
-            <img
-              src={`https://cms.samespace.com/assets/${currentSong.cover}`}
-              alt={currentSong.name}
-              className="h-full w-full object-cover"
-            />
-      </div>
-          <div ref={seekBg} className="w-[100%] bg-gray-700 rounded-full cursor-pointer mt-3" onClick={seekSong}>
-            <hr ref={seekBar} className="h-1  border-none bg-white rounded-full"/>
-          </div>
-          <div className="audio-controller mt-4 flex justify-between items-center">
-                <div className="rounded-full p-2 bg-[#2d2009] cursor-pointer backdrop-blur-2xl  hover:brightness-150 transition-all duration-300 ease-in-out">
+        <div className="h-full max-h-screen w-full flex justify-center items-start pt-[4vh] md:justify-start md:items-center md:pt-0  2xl:items-start 2xl:pt-[13vh] md:pl-28 2xl:pl-52">
+            <audio ref={audioRef} src={currentSong.url} preload="auto">
+            </audio>
+            <div className="flex flex-col text-white p-4 rounded-lg w-[280px] 2xl:w-[325px]">
+                <div className="text-left mb-4">
+                    <p className="text-xl 2xl:text-2xl font-semibold">{currentSong.name}</p>
+                    <p className="opacity-60 text-xs 2xl:text-sm">{currentSong.artist}</p>
+                </div>
+                <div className="vignette-container h-[250px] 2xl:h-[300px] w-full mb-4 rounded-md overflow-hidden">
                     <img
-                        src={optionIcon}
-                        alt={"option"}
-                        className="h-4 w-4 2xl:h-5 2xl:w-5 rounded-full"
+                        src={`https://cms.samespace.com/assets/${currentSong.cover}`}
+                        alt={currentSong.name}
+                        className="h-full w-full object-cover"
                     />
                 </div>
-                <div className="controls flex justify-between items-center space-x-3">
-                    <div className="cursor-pointer backdrop-blur-2xl  hover:brightness-150 transition-all duration-300 ease-in-out" onClick={()=>previous()}>
+                <div ref={seekBg} className="w-[100%] bg-gray-700 rounded-full cursor-pointer mt-3" onClick={seekSong}>
+                    <hr ref={seekBar} className="h-1  border-none bg-white rounded-full" />
+                </div>
+                <div className="audio-controller mt-4 flex justify-between items-center">
+                    <div className="rounded-full p-2 bg-[#2d2009] cursor-pointer backdrop-blur-2xl  hover:brightness-150 transition-all duration-300 ease-in-out">
                         <img
-                            src={prevSongIcon}
-                            alt={"previous song button"}
+                            src={optionIcon}
+                            alt={"option"}
                             className="h-4 w-4 2xl:h-5 2xl:w-5 rounded-full"
                         />
                     </div>
-                    <div className="cursor-pointer backdrop-blur-2xl  hover:brightness-150 transition-all duration-300 ease-in-out" onClick={()=>playStatus?pause():play()}>
-                        {playStatus ?
+                    <div className="controls flex justify-between items-center space-x-3">
+                        <div className="cursor-pointer backdrop-blur-2xl  hover:brightness-150 transition-all duration-300 ease-in-out" onClick={() => previous()}>
                             <img
-                            src={pauseIcon}
-                            alt={"pause"}
-                            className="h-8 w-8 2xl:h-10 2xl:w-10 rounded-full shadow-lg border-1 border-gray-900"
-                        />
-                          
-                       : <img
-                            src={playIcon}
-                            alt={"play"}
-                            className="h-8 w-8 2xl:h-10 2xl:w-10 rounded-full shadow-lg border-1 border-gray-900"
-                        />}
-                        
+                                src={prevSongIcon}
+                                alt={"previous song button"}
+                                className="h-4 w-4 2xl:h-5 2xl:w-5 rounded-full"
+                            />
+                        </div>
+                        <div className="cursor-pointer backdrop-blur-2xl  hover:brightness-150 transition-all duration-300 ease-in-out" onClick={() => playStatus ? pause() : play()}>
+                            {playStatus ?
+                                <img
+                                    src={pauseIcon}
+                                    alt={"pause"}
+                                    className="h-8 w-8 2xl:h-10 2xl:w-10 rounded-full shadow-lg border-1 border-gray-900"
+                                />
+
+                                : <img
+                                    src={playIcon}
+                                    alt={"play"}
+                                    className="h-8 w-8 2xl:h-10 2xl:w-10 rounded-full shadow-lg border-1 border-gray-900"
+                                />}
+
+                        </div>
+                        <div className="cursor-pointer backdrop-blur-2xl  hover:brightness-150 transition-all duration-300 ease-in-out" onClick={() => { nextSong() }}>
+                            <img
+                                src={nextSongIcon}
+                                alt={"next song button"}
+                                className="h-4 w-4 2xl:h-5 2xl:w-5 rounded-full"
+                            />
+                        </div>
                     </div>
-                    <div className="cursor-pointer backdrop-blur-2xl  hover:brightness-150 transition-all duration-300 ease-in-out" onClick={()=>{nextSong()}}>
-                         <img
-                            src={nextSongIcon}
-                            alt={"next song button"}
+                    <div className="rounded-full p-2 bg-[#2d2009] cursor-pointer backdrop-blur-2xl  hover:brightness-150 transition-all duration-300 ease-in-out">
+                        <img
+                            src={speakerIcon}
+                            alt={"speaker button"}
                             className="h-4 w-4 2xl:h-5 2xl:w-5 rounded-full"
                         />
                     </div>
+
                 </div>
-                <div className="rounded-full p-2 bg-[#2d2009] cursor-pointer backdrop-blur-2xl  hover:brightness-150 transition-all duration-300 ease-in-out">
-                    <img
-                    src={speakerIcon}
-                    alt={"speaker button"}
-                    className="h-4 w-4 2xl:h-5 2xl:w-5 rounded-full"
-                    />
-                </div>
-                
-          </div>
+            </div>
         </div>
-      </div>
     );
 }
 
