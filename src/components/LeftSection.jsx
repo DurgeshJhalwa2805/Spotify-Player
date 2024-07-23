300ed03e0d7f20a751ebaf74a00e34a0a1e29b93
Durgesh <durgesh.jhalwa@shikhshaml.com>
2024-07-23 14:10:42 +0530

    css

--- src/components/LeftSection.jsx
+++ src/components/LeftSection.jsx
@@ -12,13 +12,13 @@ const LeftSection = (props) => {
     return (
         <div className="flex md:flex-col h-full justify-between items-center md:items-start p-4 px-8 md:px-6 2xl:p-6">
             <div className=" md:hidden  backdrop-blur-2xl  hover:brightness-150 transition-all duration-300 ease-in-out rounded-full" onClick={toggleSidebar}>
-                <img src={isSidebarOpen ? CrossIcon : PlaylistIcon} alt="Playlist Logo" className="w-8 rounded-full p-1" />
+                <img src={isSidebarOpen ? CrossIcon : PlaylistIcon} alt="Playlist Logo" className="w-10 md:w-8 rounded-full p-1" />
             </div>
             <div>
                 <img src={SpotifyLogo} alt="Spotify Logo" className="w-28 md:w-24 2xl:w-34" />
             </div>
             <div>
-                <img src={ProfileIcon} alt="User Logo" className="w-10  md:w-12 rounded-full" />
+                <img src={ProfileIcon} alt="User Logo" className="w-12  md:w-12 rounded-full" />
             </div>
         </div>
     );
